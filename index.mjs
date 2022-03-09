95fa9e0de9da79538e8830855458393afa1cf924
ayuki_j <ajjt66554@gmail.com>
2022-03-09 18:44:11 +0900

    add parse image to s3

--- index.mjs
+++ index.mjs
@@ -5,16 +5,29 @@ import fs from 'fs';
 import client from "@notionhq/client"
 import dotenv from 'dotenv';
 import {} from 'dotenv/config';
-import {NodeHtmlMarkdown} from 'node-html-markdown'
+import {NodeHtmlMarkdown} from 'node-html-markdown';
+import {markdownToBlocks} from '@tryfabric/martian';
+import * as AWS from "@aws-sdk/client-s3";
+import {v4 as uuid} from 'uuid';
+import path from 'path';
+
 
 const {JSDOM} = jsdom;
 const {Client} = client;
+const s3 = new AWS.S3({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET,
+    },
+});
 
 (async () => {
     const notion = new Client({auth: process.env.NOTION_TOKEN});
-    const nhm = new NodeHtmlMarkdown();
+    const nhm = new NodeHtmlMarkdown({keepDataImages: true});
     const files = await read_csv(process.argv[2]);
-    const articles = await files.map(file => get_article(nhm, file['URLs'], file['Categories']));
+    const articles = await Promise.all(files.map(async file => await get_article(nhm, file['URLs'], file['Categories'])));
+    articles.map(async article => !!article ? await create_notion_page(notion, article) : null);
 })();
 
 
@@ -50,3 +63,71 @@ async function get_article(nhm, url, category) {
     };
 }
 
+async function create_notion_page(notion, article) {
+    let body = markdownToBlocks(article.body);
+    body = await Promise.all(body.flatMap(async object => object.type === 'image' ? await uploadS3(object) : object));
+    const response = await notion.pages.create({
+        parent: {
+            database_id: process.env.NOTION_DB_TOKEN,
+        },
+        properties: {
+            Name: {
+                title: [
+                    {
+                        text: {
+                            content: article.title,
+                        },
+                    },
+                ],
+            },
+        },
+        children: body
+    });
+    console.log(response);
+}
+
+async function uploadS3(block) {
+    const src = block.image.external.url;
+    if (!src) return block;
+
+    const params = {
+        Bucket: process.env.S3_BUCKET_NAME,
+    }
+
+    if (/^data:/i.test(src)) {
+        const fileData = src.replace(/^data:\w+\/\w+;base64,/, '')
+        const decodedFile = Buffer.from(fileData, 'base64')
+        const fileExtension = src.toString().slice(src.indexOf('/') + 1, src.indexOf(';'))
+        const contentType = src.toString().slice(src.indexOf(':') + 1, src.indexOf(';'))
+        params['Body'] = decodedFile;
+        params['Key'] = [process.env.S3_PREFIX, uuid(), fileExtension].join('.');
+        params['ContentType'] = contentType;
+        try {
+            await s3.putObject(params).then(() => {
+                block.image.external.url = [process.env.CLOUD_FRONT_URL, params['Key']].join('/');
+            });
+            return block;
+        } catch (err) {
+            console.error('ERROR:', err);
+            return block;
+        }
+    } else {
+        try {
+            const response = await fetch(src)
+            params['ContentType'] = response.headers.get("content-type") ?? undefined;
+            params['ContentLength'] = response.headers.get("content-length") != null
+                ? Number(response.headers.get("content-length"))
+                : undefined;
+            params['Body'] = response.body;
+            params['Key'] = [process.env.S3_PREFIX, uuid(), path.extname(src)].join('');
+            await s3.putObject(params)
+                .then(() => {
+                    block.image.external.url = [process.env.CLOUD_FRONT_URL, params['Key']].join('/');
+                });
+            return block;
+        } catch (err) {
+            console.error('ERROR:', err);
+            return block;
+        }
+    }
+}
